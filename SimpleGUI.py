0f366970e10a252d21891b17ca5309dda73fc9db
Dan Rolfe <dan@rolfe.email>
2020-05-16 16:51:43 +0100

    Move to Gtk3 and minor tidy Main

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -2,20 +2,21 @@
 
 import sys
 import os
+import traceback
 import time
 import math
 import re
 
-import pygtk
-pygtk.require('2.0')
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk
 from threading import Thread
 import thread
-import pango
+from gi.repository import Pango
 
 _draw_flow_lock=thread.allocate_lock()
 
-gtk.gdk.threads_init()
+Gdk.threads_init()
 
 class gui(Thread):
 
@@ -30,9 +31,9 @@ class gui(Thread):
 
         self._n_messages=0
         self._window_title=title
-        
+
         self._can_quit=False
-        self._window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self._window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
         self._window.set_border_width(0)
         self._window.set_default_size(width, height)
         if iconfile is not None:
@@ -41,44 +42,43 @@ class gui(Thread):
         self._window.connect("destroy", self._destroy)
 
         # Vertical box to store it all
-        self._mainbox=gtk.VBox(False, 0)
+        self._mainbox=Gtk.VBox(False, 0)
         self._window.add(self._mainbox)
 
         # Frame/box for progress
-        progress_frame=gtk.Frame("Progress")
+        progress_frame=Gtk.Frame(label="Progress")
         progress_frame.set_border_width(3)
         progress_frame.show()
         self._mainbox.pack_start(progress_frame, False, False, 0)
-        self._progress_box=gtk.VBox(False, 0)
+        self._progress_box=Gtk.VBox(False, 0)
         self._progress_box.set_border_width(3)
         # Progress message
-        self._progress_label=gtk.Label()
+        self._progress_label=Gtk.Label()
         self._progress_label.show()
         # The progress bar
-        self._progress_bar=gtk.ProgressBar()
-        self._progress_bar.set_orientation(gtk.PROGRESS_LEFT_TO_RIGHT)
+        self._progress_bar=Gtk.ProgressBar()
         self._progress_box.pack_start(self._progress_bar, True, True, 0)
         self._progress_box.pack_start(self._progress_label, False, False, 5)
         progress_frame.add(self._progress_box)
 
         # Frame/box for messages
-        self._message_frame=gtk.Frame("Messages")
+        self._message_frame=Gtk.Frame(label="Messages")
         self._message_frame.set_border_width(3)
         self._message_frame.show()
         self._mainbox.pack_start(self._message_frame, True, True, 0)
-        message_listscroll = gtk.ScrolledWindow()
-        message_listscroll.set_shadow_type(gtk.SHADOW_IN)
+        message_listscroll = Gtk.ScrolledWindow()
+        message_listscroll.set_shadow_type(Gtk.ShadowType.IN)
         message_listscroll.set_border_width(3)
-        self._message_liststore=gtk.ListStore(str, str)
-        self._message_listview=gtk.TreeView(self._message_liststore)
-        cell_rend_message=gtk.CellRendererText()
-        self._message_listview.append_column( gtk.TreeViewColumn("Message",   cell_rend_message, markup=0, cell_background=1) )
+        self._message_liststore=Gtk.ListStore(str, str)
+        self._message_listview=Gtk.TreeView(self._message_liststore)
+        cell_rend_message=Gtk.CellRendererText()
+        self._message_listview.append_column( Gtk.TreeViewColumn("Message",   cell_rend_message, markup=0, cell_background=1) )
         self._message_listview.set_headers_visible(False)
         #self._message_listview.connect("row-activated", self.preview_callback, None)
         listsel=self._message_listview.get_selection()
-        #listsel.set_mode(gtk.SELECTION_MULTIPLE)
-        listsel.set_mode(gtk.SELECTION_NONE)
-        message_listscroll.add(self._message_listview)        
+        #listsel.set_mode(Gtk.SelectionMode.MULTIPLE)
+        listsel.set_mode(Gtk.SelectionMode.NONE)
+        message_listscroll.add(self._message_listview)
         self._message_frame.add(message_listscroll)
         message_listscroll.show()
         self._message_listview.show()
@@ -93,17 +93,17 @@ class gui(Thread):
     def run(self):
 
         # Main GUI loop
-        gtk.main()
+        Gtk.main()
 
     def new_message(self, message):
 
         colour=["white", "grey90"][self._n_messages % 2]
 
-        gtk.gdk.threads_enter()
+        Gdk.threads_enter()
         message_iter=self._message_liststore.append([message, colour])
         message_path=self._message_liststore.get_path(message_iter)
         self._message_listview.scroll_to_cell(message_path)
-        gtk.gdk.threads_leave()
+        Gdk.threads_leave()
 
         self._n_messages+=1
 
@@ -123,29 +123,30 @@ class gui(Thread):
     def enable_quit(self):
 
         self._can_quit=True
-    
+
     def close(self):
 
         self._window.destroy()
-        gtk.main_quit()
+        Gtk.main_quit()
 
     def set_frac(self, frac):
 
-        gtk.gdk.threads_enter()
+        Gdk.threads_enter()
         self._progress_bar.set_fraction(frac)
         self._window.set_title("%s - %3d%%" % (self._window_title, int(frac*100)))
-        gtk.gdk.threads_leave()
+        Gdk.threads_leave()
 
     def set_text(self, text):
-        
-        gtk.gdk.threads_enter()
+
+        Gdk.threads_enter()
         self._progress_label.set_text(text)
-        gtk.gdk.threads_leave()
+        Gdk.threads_leave()
 
-        
-if __name__ == "__main__":
 
+def Test():
     import time
+
+    g=None
 
     try:
 
@@ -153,7 +154,7 @@ if __name__ == "__main__":
         g.start()
 
         #g.read_flow("program_flow.plain")
-        
+
         ntest=100
         for i in range(ntest):
             g.set_text("Doing %d of %d ..." % (i, ntest))
@@ -173,14 +174,20 @@ if __name__ == "__main__":
             if i==45:
                 g.new_message("A more interesting message. Not.")
 
-    except Exception, mess:
-
-        g.new_message("<b>"+str(mess)+"</b>")
-
+    except Exception as err:
+        #    g.new_message("<b>"+str(err)+"</b>")
+        print("Fatal error: "+str(err))
+        print("\n--- Failure -------------------------------------")
+        traceback.print_exc(file=sys.stdout)
+        print("-------------------------------------------------------")
     #finally:
 
-    g.enable_quit()
-    g.new_message("<b>Waiting for you to close the window.....</b>")
-    g.join()
+    if g is not None:
+        g.enable_quit()
+        g.new_message("<b>Waiting for you to close the window.....</b>")
+        g.join()
 
-    print "Thankyou. Finished"
+    print("Thankyou. Finished")
+
+if __name__ == "__main__":
+    Test()
